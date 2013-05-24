3652c783d4d98c2921e2bff421992cab07a98132
chaosfoal <arturs.znotins@gmail.com>
2013-05-24 12:35:31 +0300

    modified running scripts

--- lvcoref.bat
+++ lvcoref.bat
@@ -1,2 +1,2 @@
 @echo off
-java -Xmx1G -Dfile.encoding=UTF-8 -cp dist\lvcoref.jar;lib\commons-lang3-3.1.jar LVCoref.LVCoref %*
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\lvcoref.jar;lib/*" LVCoref.LVCoref %*
